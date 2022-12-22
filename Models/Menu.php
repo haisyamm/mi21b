b491ee81734834c8e91f5bce5252556f7553fa34
haisyamm <70573490+haisyamm@users.noreply.github.com>
2022-12-22 11:02:31 +0700

    Add files via upload

--- Models/Menu.php
+++ Models/Menu.php
@@ -9,15 +9,14 @@ class Menu extends Model
     protected $table      = 'menus';
 
     public function main_menu(){
-        return $this->where('menu_level', 0)->findAll();
+        return $this->where('menu_level', 0)->where('is_active', 1)->orderBy('orderCol', 'ASC')->findAll();
     }
 
-    public function sub_menu($id){
-        return $this->where('menu_level', 1)->where('parent_id', $id)->findAll();
+    public function sub_menu($parent_id){
+        return $this->where('menu_level', 1)->where('parent_id', $parent_id)->findAll();
     }
 
     public function sub_sub_menu(){
         return $this->where('menu_level', 2)->findAll();
     }
-    
 }
